ebcd0837d9e0ba38262c5c49bbe934e58920f4d3
fm1308-web <fm1308@gmail.com>
2026-08-23 12:57:05 +1000

    g

--- shell.js
+++ shell.js
@@ -404,7 +404,7 @@ function loadAndMountView(name){
     return true;
   }
   // load CSS once (with cache-bust to defeat browser caching during dev)
-  var _cb = '?v=187';
+  var _cb = '?v=188';
   if(cfg.css){
     var l = document.createElement('link');
     l.rel = 'stylesheet';
